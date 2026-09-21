40dc6c6ed9444de7f31e134d0c6a95c5b04382bb
andreiaalpe <andreiaalpe@gmail.com>
2026-09-21 12:54:36 -0300

    OK

---
+++ app1.py
@@ -0,0 +1,16 @@
+# Recebe o nome do usuário na variável
+name = input("Qual o seu nome? ")
+
+# Recebe a idade do usuário na variável
+age = input (" Qual a sua idade? ")
+
+# Exibe uma mensagem formatada
+print("Olá", name, "!")
+print("Você tem", age, "anos")
+
+
+# usando string formatada
+
+print()   
+print(f"Olá {name}! Você tem {age} anos.")
+
---
+++ app2.py
@@ -0,0 +1,13 @@
+name = input ("QUal o seu nome? ")
+age = input("qual a sua idade?")
+
+print(type(name))
+print(type (age))
+
+age = int(age)
+
+older = age + 10
+print(f"{name} terá {older}daqui a 10 anos.")
+
+      
+
